9e2fa85443150576d2200750e493af7aa774662d
Isaac Serrano <g11ada19@hackademy.mx>
2020-12-06 13:40:37 -0700

    Migrado a b4 amenidades y galería

--- stirealstate/src/application/views/home/amenidades-info.php
+++ stirealstate/src/application/views/home/amenidades-info.php
@@ -1,4 +1,4 @@
-<div class="amenidades-info" id="amenidades">
+<div class="amenidades-info pt-4" id="amenidades">
   <div class="container">
     <div class="row">
       <div class="col-md-12">
@@ -9,20 +9,28 @@
         <div class="swiper-container-multiple-4">
           <div class="swiper-wrapper">
             <div class="swiper-slide">
-              <img class="img-responsive" alt="" src="http://placehold.it/400x320">
-                <h3>Amenidad 1</h3>
+              <a href="#" class=".text-decoration-none">
+                <img class="img-fluid" alt="" src="http://placehold.it/400x320">
+                <h3 class=".text-decoration-none" style="color: #2c2c2c;">Amenidad 1</h3>
+              </a>
             </div>
             <div class="swiper-slide">
-              <img class="img-responsive" alt="" src="http://placehold.it/400x320">
-                <h3>Amenidad 2</h3>
+              <a href="#" class=".text-decoration-none">
+                <img class="img-fluid" alt="" src="http://placehold.it/400x320">
+                <h3 class=".text-decoration-none" style="color: #2c2c2c;">Amenidad 2</h3>
+              </a>
             </div>
             <div class="swiper-slide">
-              <img class="img-responsive" alt="" src="http://placehold.it/400x320">
-                <h3>Amenidad 3</h3>
+              <a href="#" class=".text-decoration-none">
+                <img class="img-fluid" alt="" src="http://placehold.it/400x320">
+                <h3 class=".text-decoration-none" style="color: #2c2c2c;">Amenidad 3</h3>
+              </a>
             </div>
             <div class="swiper-slide">
-              <img class="img-responsive" alt="" src="http://placehold.it/400x320">
-                <h3>Amenidad 4</h3>
+              <a href="#" class=".text-decoration-none">
+                <img class="img-fluid" alt="" src="http://placehold.it/400x320">
+                <h3 class=".text-decoration-none" style="color: #2c2c2c;">Amenidad 4</h3>
+              </a>
             </div>
           </div>
         </div>
