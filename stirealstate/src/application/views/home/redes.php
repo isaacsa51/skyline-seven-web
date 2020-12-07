afe9af7d41095b0dce0b972a2a0ed962bf5cadce
Isaac Serrano <g11ada19@hackademy.mx>
2020-12-06 17:22:08 -0700

    Redes y Footer migrados y responsivas

--- stirealstate/src/application/views/home/redes.php
+++ stirealstate/src/application/views/home/redes.php
@@ -1,25 +1,25 @@
 <div class="redes">
     <div class="container">
         <div class="row">
-            <h1 class="text-center">Síguenos en nuestras redes</h1>
+            <h1 class="text-center col-12">Síguenos en nuestras redes</h1>
 
             <div class="col-md-2 col-sm-4 col-xs-6">
-                <img src="https://placehold.it/500x500" class="img-responsive" alt="...">
+                <img src="https://placehold.it/500x500" class="img-fluid" alt="...">
             </div>
             <div class="col-md-2 col-sm-4 col-xs-6">
-                <img src="https://placehold.it/500x500" class="img-responsive" alt="...">
+                <img src="https://placehold.it/500x500" class="img-fluid" alt="...">
             </div>
             <div class="col-md-2 col-sm-4 col-xs-6">
-                <img src="https://placehold.it/500x500" class="img-responsive" alt="...">
+                <img src="https://placehold.it/500x500" class="img-fluid" alt="...">
             </div>
             <div class="col-md-2 col-sm-4 col-xs-6">
-                <img src="https://placehold.it/500x500" class="img-responsive" alt="...">
+                <img src="https://placehold.it/500x500" class="img-fluid" alt="...">
             </div>
             <div class="col-md-2 col-sm-4 col-xs-6">
-                <img src="https://placehold.it/500x500" class="img-responsive" alt="...">
+                <img src="https://placehold.it/500x500" class="img-fluid" alt="...">
             </div>
             <div class="col-md-2 col-sm-4 col-xs-6">
-                <img src="https://placehold.it/500x500" class="img-responsive" alt="...">
+                <img src="https://placehold.it/500x500" class="img-fluid" alt="...">
             </div>
         </div>
     </div>
